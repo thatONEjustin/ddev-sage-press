88b6f5d80b2453d7d0b918cfa5e07febb8e8ed2a
Wojojustin <42546444+WojoJustin@users.noreply.github.com>
2025-06-03 18:43:35 -0500

    initial sage commit

---
+++ wp-content/themes/ddev-sage-press/vite.config.js
@@ -0,0 +1,53 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
+import laravel from 'laravel-vite-plugin'
+import { wordpressPlugin, wordpressThemeJson } from '@roots/vite-plugin';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const SITE_URL = (process.env.DDEV_PRIMARY_URL) ? process.env.DDEV_PRIMARY_URL : env.WP_SITE_URL;
+
+  return {
+    base: '/wp-content/themes/nyc-vein-fibroid/public/build/',
+    server: {
+      host: '0.0.0.0',
+      allowedHosts: [
+        '.ddev.site',
+      ],
+      origin: `${SITE_URL.replace(/:\d+$/, "")}:5173`,
+      cors: {
+        origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
+      },
+    },
+    plugins: [
+      tailwindcss(),
+      laravel({
+        input: [
+          'resources/css/app.css',
+          'resources/js/app.js',
+          'resources/css/editor.css',
+          'resources/js/editor.js',
+        ],
+        refresh: true,
+      }),
+
+      wordpressPlugin(),
+
+      // Generate the theme.json file in the public/build/assets directory
+      // based on the Tailwind config and the theme.json file from base theme folder
+      wordpressThemeJson({
+        disableTailwindColors: false,
+        disableTailwindFonts: false,
+        disableTailwindFontSizes: false,
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@scripts': '/resources/js',
+        '@styles': '/resources/css',
+        '@fonts': '/resources/fonts',
+        '@images': '/resources/images',
+      },
+    },
+  }
+})
